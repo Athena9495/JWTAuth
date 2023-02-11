e94539eae85a69dacb87de05ee814aa11eea5e94
Athena9495 <28779920+Athena9495@users.noreply.github.com>
2023-02-12 03:19:32 +0530

    Add project files.

---
+++ JWTAuth/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace JWTAuth.Models
+{
+    public class User
+    {
+        public String UserName { get; set; } = string.Empty;
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+    }
+}
---
+++ JWTAuth/Models/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace JWTAuth.Models
+{
+    public class UserDto
+    {
+        public String UserName { get; set; } = string.Empty;
+
+        public String Password { get; set; } = string.Empty;
+
+    }
+}
